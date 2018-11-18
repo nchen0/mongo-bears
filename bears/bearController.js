56632841be6b2b31f506080d55ccfdb6c0ecb725
nchen0 <nicky.chen0@gmail.com>
2018-11-18 14:25:35 -0800

    full CRUD operations

--- bears/bearController.js
+++ bears/bearController.js
@@ -1,39 +1,80 @@
 const router = require("express").Router();
 const Bear = require("./bearModel");
 
-router
-  .route("/")
-  .get((req, res) => {
-    Bear.find()
-      .then(foundBears => {
-        res.status(200).json(foundBears);
-      })
-      .catch(err => {
-        res.status(404).json({ error: err.message });
-      });
+router.get("/", async (req, res) => {
+  try {
+    const foundBears = await Bear.find();
+    res.status(200).json(foundBears);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+  /* Promise way.
+  Bear.find()
+  .then(foundBears => {
+    res.status(200).json(foundBears);
   })
-  .post((req, res) => {
-    const newBear = new Bear(req.body);
-    newBear
-      .save() // this will `insert` a document into the Bear collection
-      .then(savedBear => {
-        res.status(201).json(savedBear);
-      })
-      .catch(err => {
-        res.status(404).json({ error: err.message });
-      });
-  });
+  .catch(err => {
+    res.status(404).json({ error: err.message });
+  });*/
+});
 
+router.post("/", (req, res) => {
+  const newBear = new Bear(req.body);
+  newBear
+    .save() // this will `insert` a document into the Bear collection
+    .then(savedBear => {
+      res.status(201).json(savedBear);
+    })
+    .catch(err => {
+      res.status(400).json({ error: err.message });
+    });
+});
+
+// Different syntax of routes:
+
+// The two checks for below are because the 1st (outside catch), checks if the objectID is valid, not if it exists, because if it's valid it'll return a success either way even if it doesn't exist.
 router
   .route("/:id")
-  .get((req, res) => {
-    res.status(200).json({ route: "/api/bears/" + req.params.id });
+  .get(async (req, res) => {
+    try {
+      const singleBear = await Bear.findById(req.params.id);
+      if (singleBear) {
+        res.status(200).json(singleBear);
+      } else {
+        res.status(400).json("Not found by ID");
+      }
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
   })
   .delete((req, res) => {
-    res.status(200).json({ status: "please implement DELETE functionality" });
+    const { id } = req.params;
+    Bear.findByIdAndRemove(id) // This funciton is a mongoose function.
+      .then(bear => {
+        if (bear) {
+          res.status(200).json(bear);
+        } else {
+          res.status(400).json("Not found by ID");
+        }
+      })
+      .catch(err => res.status(500).json(err.message));
   })
-  .put((req, res) => {
-    res.status(200).json({ status: "please implement PUT functionality" });
+  .put(async (req, res) => {
+    const { id } = req.params;
+
+    if (!req.body.species && !req.body.latinName) {
+      return res.status(400).json("Please put something in the body to change.");
+    }
+    try {
+      const newBear = await Bear.findByIdAndUpdate(id, req.body);
+      if (newBear) {
+        res.status(200).json(newBear);
+      } else {
+        res.status(404).json("Not found by ID");
+      }
+    } catch (err) {
+      res.status(400).json(err.message);
+    }
   });
 
 module.exports = router;
--- bears/bearModel.js
+++ bears/bearModel.js
@@ -2,22 +2,29 @@
 const mongoose = require("mongoose");
 // Schema
 
-const BearSchema = new mongoose.Schema({
-  // declare a schema. Your documents will 'look like this'
-  species: {
-    type: String,
-    required: true,
-    unique: true
+const BearSchema = new mongoose.Schema(
+  {
+    // declare a schema. Your documents will 'look like this'
+    species: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    latinName: {
+      type: String,
+      require: true
+    },
+    createOn: {
+      type: Date, // Date is a nother type in Mongoose.
+      default: Date.now()
+    }
   },
-  latinName: {
-    type: String,
-    require: true
-  },
-  createOn: {
-    type: Date, // Date is a nother type in Mongoose.
-    default: Date.now()
+  // Below are the options. Optional, doesn't HAVE to have it.
+  {
+    timestamps: true,
+    strict: false // If we want to put new stuff, extra schema.
   }
-});
+);
 
 const bearsModel = mongoose.model("Bear", BearSchema);
 
